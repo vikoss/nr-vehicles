5253ff31040e2e65c6b8cfcf4f965eed67714d96
vikoss <vikoss.yiss@gmail.com>
2022-05-25 12:07:36 -0500

    add include facades inside the services provider

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -209,7 +210,11 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
     ])->toArray(),
 
 ];
