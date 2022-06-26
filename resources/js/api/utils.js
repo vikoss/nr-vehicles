ace2a2b9558c4a8da78d6fa964da7eeed5002daa
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:49:12 -0500

    add services for generic utilities

---
+++ resources/js/api/utils.js
@@ -0,0 +1,19 @@
+import { post } from 'axios'
+import { JWT } from '../helpers/localStorage'
+import { API } from './baseUrl'
+
+const uploadFile = ({ path, file }) => new Promise((resolve, reject) => {
+  const body = new FormData()
+  body.append('path', path)
+  body.append('file', file)
+
+  post(`${API}/api/upload-file`, body, {
+    headers: {
+      Authorization: `Bearer ${JWT()}`,
+    },
+  })
+  .then(({ data }) => resolve(data))
+  .catch(({ response }) => reject(response))
+})
+
+export { uploadFile }
