d1e6180dfdb389a00decede4ae5179dfa2b8ebe0
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:44:54 -0500

    add model success web component

---
+++ resources/js/components/ModalSuccess.vue
@@ -0,0 +1,58 @@
+<template>
+  <modal-base :show="show" @closed="closed">
+    <div class="py-12 pb-8">
+      <div class="flex flex-col items-center">
+        <img
+          src="./../assets/svg/success.svg"
+          alt="Carpeta"
+          style="width: 152px;"
+        >
+        <h5 class="text-base mb-14 mt-12 text-center" :style="'max-width: 587px; min-width: ' + minWidth ">
+          {{ message }}
+        </h5>
+      </div>
+      <button-base
+        class="max-w-xs mx-auto"
+        label="Aceptar"
+        @click="closed"
+      />
+    </div>
+  </modal-base>
+</template>
+
+<script>
+import ModalBase from './ModalBase.vue'
+import ButtonBase from './ButtonBase.vue'
+
+export default {
+  components: {
+    ModalBase,
+    ButtonBase,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    closed: {
+      type: Function,
+      default: () => {},
+    },
+    minWidth: {
+      type: String,
+      default: 'auto',
+    },
+  },
+  emits: ['closed'],
+  setup() {
+    const close = () => this.$emit('closed')
+
+    return { close }
+  },
+
+}
+</script>
