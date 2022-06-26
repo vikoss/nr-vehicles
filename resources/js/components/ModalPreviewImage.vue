3c2ad62d04e612fef678dd4042daa9eb456706dd
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:44:34 -0500

    add model preview image web component

---
+++ resources/js/components/ModalPreviewImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <modal-base :show="show" @closed="closed">
+    <div class="max-w-xl lg:max-w-4xl flex justify-center">
+      <img style="max-height: 85vh;" :src="img" alt="Documento">
+    </div>
+  </modal-base>
+</template>
+
+<script>
+import ModalBase from './ModalBase.vue'
+
+export default {
+  name: 'ModalPreviewImage',
+  components: { ModalBase },
+  props: {
+    img: {
+      type: String,
+      default: '',
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['closed'],
+  setup(props, { emit }) {
+
+    const closed = () => {
+      emit('closed')
+    }
+
+    return { closed }
+  },
+}
+</script>
