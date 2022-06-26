a505ff7997929f647fd0de8666c7dc5d1e089452
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:57:48 -0500

    add search view

--- resources/js/views/Search.vue
+++ resources/js/views/Search.vue
@@ -8,9 +8,9 @@
     />
     <table-base
       v-show="app.isEmpty"
-      :headers="[{ name: 'economic_number', label: 'Economico' }, { name: 'name', label: 'Nombre' }, { name: 'inventory_number', label: 'Inventario' }, { name: 'serial_number', label: 'Número de serie' }, ]"
+      :headers="[{ name: 'economic_number', label: 'Economico' }, { name: 'name', label: 'Nombre' }, { name: 'inventory_number', label: 'Inventario' } ]"
       :data="app.vehicles.data"
-      :action="() => ({})"
+      :action="app.goVehicleDetails"
     />
   </main>
 </template>
@@ -18,7 +18,7 @@
 <script>
 import { computed, reactive } from 'vue'
 import { useRouter } from 'vue-router'
-import { searchVehicles } from '../api/vechicles'
+import { searchVehicles, getVehicles } from '../api/vehicles'
 import HeaderBase from './../components/HeaderBase.vue'
 import SearchBar from '../components/SearchBar.vue'
 import TableBase from './../components/TableBase.vue'
@@ -26,6 +26,8 @@ import TableBase from './../components/TableBase.vue'
 export default {
   components: { HeaderBase, SearchBar, TableBase },
   setup() {
+    const router = useRouter()
+
     const app = reactive({
       value: '',
       vehicles: [],
@@ -37,8 +39,14 @@ export default {
       }),
       getVehicles: async () => {
         app.vehicles = await searchVehicles(app.value)
-      }
+      },
+      getAllVehicles: async () => {
+        app.vehicles = await getVehicles()
+      },
+      goVehicleDetails: (vehicle) => router.push({ name: 'VehicleDetail', params: { vehicle }})
     })
+
+    app.getAllVehicles()
 
     return { app }
   },
