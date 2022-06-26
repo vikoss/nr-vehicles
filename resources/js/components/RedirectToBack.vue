f844a59e71d07f810c52d80292b2f272c7fc417f
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:42:57 -0500

    add redirect to back web component

---
+++ resources/js/components/RedirectToBack.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="goBack" class="flex items-center cursor-pointer">
+    <arrow-left-svg />
+    <p class="text-wine text-lg ml-2">Regresar</p>
+  </div>
+</template>
+
+<script>
+import { useRouter } from "vue-router"
+import ArrowLeftSvg from "../svg/ArrowLeft.vue"
+
+export default {
+  name: 'RedirectToBack',
+  components: { ArrowLeftSvg },
+  setup() {
+    const router = useRouter()
+    const goBack = () => router.go(-1)
+
+    return { goBack }
+  },
+}
+</script>
