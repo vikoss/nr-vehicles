c399680bab197e7dd467c758ae02c44c00ffe0ec
vikoss <vikoss.yiss@gmail.com>
2022-06-25 19:41:26 -0500

    add svg arrow rigth web component

---
+++ resources/js/svg/ArrowRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="62.21" height="100.75" viewBox="0 0 6.221 10.075">
+    <path :style="`fill: ${hexColor}`" d="M14.891,8.59l-3.854,3.845L7.184,8.59,6,9.774l5.038,5.038,5.038-5.038Z" transform="translate(-8.59 16.075) rotate(-90)" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ArrowLeft',
+  props: {
+    hexColor: {
+      type: String,
+      default: '#54191b',
+    },
+  },
+}
+</script>
