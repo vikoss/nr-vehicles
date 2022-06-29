9c1bacee5de3e2c944c1775936e873e2f1ef9741
vikoss <vikoss.yiss@gmail.com>
2022-06-29 00:11:31 -0500

    add values for disabled user scalable

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
