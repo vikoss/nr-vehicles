bf6a1233fc8e9abcc2bc1a0b260cae4dd23ede4b
vikoss <vikoss.yiss@gmail.com>
2022-06-29 00:13:13 -0500

    add service for get all vehicle documents

--- resources/js/api/documents.js
+++ resources/js/api/documents.js
@@ -12,4 +12,14 @@ const storeDocument = ({ vehicleId, document }) => new Promise((resolve, reject)
   .catch(({ response }) => reject(response))
   )
 
-export { storeDocument }
+const getDocuments = (vehicleId) => new Promise((resolve, reject) =>
+  get(`${API}/api/vehicles/${vehicleId}/documents`, {
+    headers: {
+      Authorization: `Bearer ${JWT()}`,
+    },
+  })
+  .then(({ data }) => resolve(data))
+  .catch(({ response }) => reject(response))
+  )
+
+export { storeDocument, getDocuments }
