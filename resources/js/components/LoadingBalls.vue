f79cfb19e993821008990df5bdf083e495f12935
vikoss <vikoss.yiss@gmail.com>
2022-06-30 20:10:23 -0500

    add loading balls web component

---
+++ resources/js/components/LoadingBalls.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="width: 230px; height: 150px; display: flex; flex-wrap: wrap; align-items: flex-end; justify-content:space-between;">
+    <div class="bg-wine ball"></div>
+    <div class="bg-wine ball"></div>
+    <div class="bg-wine ball"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.ball {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  animation: bounce .5s alternate infinite;
+}
+
+.ball:nth-child(2) {
+  animation-delay: .16s;
+}
+
+.ball:nth-child(3) {
+  animation-delay: .32s;
+}
+@keyframes bounce {
+  from {
+    transform: scaleX(1.25);
+  }
+  to {
+    transform: translateY(-100px) scale(1);
+  }
+}
+
+</style>
