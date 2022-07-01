5de5ad145c5a5a8928a68c0f79a9148df7d198cb
vikoss <vikoss.yiss@gmail.com>
2022-06-30 20:11:40 -0500

    add query params for get vehicles service

--- resources/js/api/vehicles.js
+++ resources/js/api/vehicles.js
@@ -2,18 +2,8 @@ import { post, get, put } from 'axios'
 import { JWT } from '../helpers/localStorage'
 import { API } from './baseUrl'
 
-const searchVehicles = (value) => new Promise((resolve, reject) =>
-  get(`${API}/api/vehicles?economic_number=${value}`, {
-    headers: {
-      Authorization: `Bearer ${JWT()}`,
-    },
-  })
-  .then(({ data }) => resolve(data))
-  .catch(({ response }) => reject(response))
-  )
-
-const getVehicles = () => new Promise((resolve, reject) =>
-  get(`${API}/api/vehicles`, {
+const searchVehicles = ({ economicNumber, page }) => new Promise((resolve, reject) =>
+  get(`${API}/api/vehicles?page=${page}${economicNumber ? '&economic_number='+economicNumber : ''}`, {
     headers: {
       Authorization: `Bearer ${JWT()}`,
     },
@@ -32,4 +22,4 @@ const getVehicle = (vehicle) => new Promise((resolve, reject) =>
   .catch(({ response }) => reject(response))
   )
 
-export { searchVehicles, getVehicles, getVehicle }
+export { searchVehicles, getVehicle }
