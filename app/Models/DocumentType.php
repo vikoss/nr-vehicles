caf2578ae1d0e855f9889529a4ba9a3d1b7c5893
vikoss <vikoss.yiss@gmail.com>
2022-07-01 19:20:20 -0500

    add getter for timestamps attributes on document type model

--- app/Models/DocumentType.php
+++ app/Models/DocumentType.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class DocumentType extends Model
 {
     use HasFactory;
+
+    public function getCreatedAtAttribute($date)
+    {
+        return (bool) $date ? Carbon::createFromDate($date)->format('Y-m-d H:i') : null;
+    }
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return (bool) $date ? Carbon::createFromDate($date)->format('Y-m-d H:i') : null;
+    }
 }
