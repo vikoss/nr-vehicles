2011d7aed036ab7a906c902b35c074499cb72aba
vikoss <vikoss.yiss@gmail.com>
2022-07-01 19:25:48 -0500

    add svg assets arrows

--- resources/js/svg/ArrowLeft.vue
+++ resources/js/svg/ArrowLeft.vue
@@ -1,10 +1,8 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <defs>
-    </defs>
-    <g transform="translate(24 24) rotate(180)">
-      <path style="fill:none;" d="M0,0H24V24H0Z"/>
-      <path :style="`fill: ${hexColor}`" d="M22,12,18,8v3H3v2H18v3Z"/>
+    <g id="Grupo_18500" data-name="Grupo 18500" transform="translate(-980 -696)">
+      <rect id="Bounds" width="24" height="24" transform="translate(980 696)" fill="none"/>
+      <path id="Icon" d="M15.4,7.4,14,6,8,12l6,6,1.4-1.4L10.8,12Z" transform="translate(980 696)" :fill="hexColor"/>
     </g>
   </svg>
 </template>
--- resources/js/svg/ArrowRight.vue
+++ resources/js/svg/ArrowRight.vue
@@ -1,12 +1,15 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="62.21" height="100.75" viewBox="0 0 6.221 10.075">
-    <path :style="`fill: ${hexColor}`" d="M14.891,8.59l-3.854,3.845L7.184,8.59,6,9.774l5.038,5.038,5.038-5.038Z" transform="translate(-8.59 16.075) rotate(-90)" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <g id="Grupo_18849" data-name="Grupo 18849" transform="translate(1004 720) rotate(180)">
+      <rect id="Bounds" width="24" height="24" transform="translate(980 696)" fill="none"/>
+      <path id="Icon" d="M15.4,7.4,14,6,8,12l6,6,1.4-1.4L10.8,12Z" transform="translate(980 696)" :fill="hexColor"/>
+    </g>
   </svg>
 </template>
 
 <script>
 export default {
-  name: 'ArrowLeft',
+  name: 'ArrowRigth',
   props: {
     hexColor: {
       type: String,
