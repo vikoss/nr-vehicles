18db64c283947557a55dc7dda962c5a98472d014
vikoss <vikoss.yiss@gmail.com>
2022-07-05 15:10:21 -0500

    change columns for user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nick_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            /* $table->foreignId('role_id')->constrained('roles')->nullable(); */
+            $table->foreignId('employee_id')->nullable()->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
