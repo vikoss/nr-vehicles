99ba15c8aa6816e94865a4ff7c30296fc9fb2e84
vikoss <vikoss.yiss@gmail.com>
2022-07-04 23:27:24 -0500

    add iframe for pdf file view in preview image web component

--- resources/js/components/PreviewImage.vue
+++ resources/js/components/PreviewImage.vue
@@ -3,10 +3,22 @@
     <div>
       <div class="h-20 w-20 flex justify-center items-center" style="background-color: #F1F1F1;">
         <img
+          v-show="documentType.includes('image')"
           class="max-h-20"
           :src="img === 'default' ? PreviewDefaultImage : img"
           alt="landscape"
         >
+        <div v-show="documentType.includes('pdf')" class="w-11/12">
+          <div
+            class="h-20"
+            style="position: relative; overflow: hidden; width: 100%;"
+          >
+            <iframe
+              style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%;"
+              :src="img"
+            />
+          </div>
+        </div>
       </div>
       <div class="flex justify-between items-center mt-3">
         <img
@@ -26,7 +38,12 @@
         >
       </div>
     </div>
-    <modal-preview-image :show="app.modal" :img="img" @closed="app.toggleModal(false)" />
+    <modal-preview-image
+      :show="app.modal"
+      :url-file="img"
+      :document-type="documentType"
+      @closed="app.toggleModal(false)"
+    />
   </div>
 </template>
 
@@ -51,13 +68,24 @@ export default {
       type: Boolean,
       default: false,
     },
+    documentType: {
+      type: String,
+      default: 'image/png',
+    },
   },
   emits: ['deleteImage'],
   setup(props, { emit }) {
     const app = reactive({
       modal: false,
       toggleModal: (status) => (app.modal = status),
-      deleteImage: () => emit('deleteImage', props.documentIndex)
+      deleteImage: () => emit('deleteImage', props.documentIndex),
+      /* showPreview: () => {
+        if (props.documentType === 'pdf') {
+          wind
+          return
+        }
+        app.toggleModal(true)
+      }, */
     })
 
     return { app, PreviewDefaultImage }
