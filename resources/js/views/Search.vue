7a09c557d6a575febf05ab05035b91f5cff757c1
vikoss <vikoss.yiss@gmail.com>
2022-07-04 23:28:06 -0500

    add search view

--- resources/js/views/Search.vue
+++ resources/js/views/Search.vue
@@ -1,52 +1,81 @@
 <template>
   <header-base />
-  <main class="px-6 sm:px-16 py-12">
+  <main>
     <search-bar
-      v-model="app.value"
-      @search="app.getVehicles"
       id="search-bar"
+      v-model="app.economicNumber"
+      @search="app.fetchVehicles"
+      placeholder="Buscar por número economico"
     />
-    <table-base
-      v-show="app.isEmpty"
-      :headers="[{ name: 'economic_number', label: 'Economico' }, { name: 'name', label: 'Nombre' }, { name: 'inventory_number', label: 'Inventario' } ]"
-      :data="app.vehicles.data"
-      :action="app.goVehicleDetails"
-    />
+    <div class="px-6 sm:px-16 py-12">
+      <redirect-to-back :route="app.goToHome" />
+      <title-bar
+        title="Vehículos"
+        subtitle="Consulta la información del vehiculo."
+        style="margin-bottom: 1.2rem;"
+      />
+      <div v-show="!!app.vehicles.data.length && !app.loading">
+        <table-base
+          :headers="[{ name: 'economic_number', label: 'Economico' }, { name: 'name', label: 'Nombre' }, { name: 'inventory_number', label: 'Inventario' } ]"
+          :data="app.vehicles.data"
+          :action="app.goToVehicleDetails"
+        />
+        <pagination-base
+          :currentPage="app.vehicles.current_page"
+          :offset="4"
+          :lastPage="app.vehicles.last_page"
+          @pageChanged="app.fetchVehicles"
+          class="float-right mt-5"
+        />
+      </div>
+      <loading v-show="app.loading" class="mx-auto mt-10" />
+    </div>
   </main>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
+import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
-import { searchVehicles, getVehicles } from '../api/vehicles'
+import { searchVehicles } from '../api/vehicles'
 import HeaderBase from './../components/HeaderBase.vue'
 import SearchBar from '../components/SearchBar.vue'
 import TableBase from './../components/TableBase.vue'
+import Loading from '../components/LoadingBalls.vue'
+import PaginationBase from '../components/PaginationBase.vue'
+import RedirectToBack from './../components/RedirectToBack.vue'
+import TitleBar from './../components/TitleBar.vue'
 
 export default {
-  components: { HeaderBase, SearchBar, TableBase },
+  components: {
+    HeaderBase,
+    SearchBar,
+    TableBase,
+    Loading,
+    PaginationBase,
+    RedirectToBack,
+    TitleBar,
+  },
   setup() {
     const router = useRouter()
 
     const app = reactive({
-      value: '',
-      vehicles: [],
-      isEmpty: computed(() => {
-        if (app.vehicles.data) {
-          return !!app.vehicles.data.length
-        }
-        return false
-      }),
-      getVehicles: async () => {
-        app.vehicles = await searchVehicles(app.value)
+      economicNumber: '',
+      vehicles: {
+        data: [],
+        current_page: 1,
+        last_page: 1,
       },
-      getAllVehicles: async () => {
-        app.vehicles = await getVehicles()
+      loading: false,
+      fetchVehicles: async (page = 1) => {
+        app.loading = true
+        app.vehicles = await searchVehicles({ economicNumber: app.economicNumber, page })
+        app.loading = false
       },
-      goVehicleDetails: (vehicle) => router.push({ name: 'VehicleDetail', params: { vehicle }})
+      goToVehicleDetails: (vehicle) => router.push({ name: 'VehicleDetail', params: { vehicle }}),
+      goToHome: () => router.push({ name: 'Home' }),
     })
 
-    app.getAllVehicles()
+    app.fetchVehicles()
 
     return { app }
   },
