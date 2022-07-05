ff7c4823a6caaca68e726539cbb49fe1f0a7de79
vikoss <vikoss.yiss@gmail.com>
2022-07-05 15:06:37 -0500

    add employee migration

---
+++ database/migrations/2013_07_05_153250_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('first_surname');
+            $table->string('second_surname');
+            $table->string('employee_number')->unique();
+            $table->foreignId('position_id')->constrained('positions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
