e7148f7db895d41aadd48db516d9fc60d348ced1
vikoss <vikoss.yiss@gmail.com>
2022-07-04 22:50:21 -0500

    add modal confirm web component

---
+++ resources/js/components/ModalConfirm.vue
@@ -0,0 +1,70 @@
+<template>
+  <modal-base :show="show" @closed="closed">
+    <div class="py-12 pb-8">
+      <div class="flex flex-col items-center">
+        <img
+          src="./../assets/svg/success.svg"
+          alt="Carpeta"
+          style="width: 152px;"
+        >
+        <h5 class="text-base mb-14 mt-12 text-center" :style="'max-width: 587px; min-width: ' + minWidth ">
+          {{ message }}
+        </h5>
+      </div>
+      <div class="flex justify-center">
+        <button-base
+          class="max-w-xs mr-2 text-wine border-solid border-wine"
+          style="background-color: #ffffff; border-width: 3px;"
+          label="No"
+          @click="closed"
+        />
+        <button-base
+          class="max-w-xs ml-2"
+          label="Si"
+          @click="action"
+        />
+      </div>
+    </div>
+  </modal-base>
+</template>
+
+<script>
+import ModalBase from './ModalBase.vue'
+import ButtonBase from './ButtonBase.vue'
+
+export default {
+  components: {
+    ModalBase,
+    ButtonBase,
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    closed: {
+      type: Function,
+      default: () => {},
+    },
+    minWidth: {
+      type: String,
+      default: 'auto',
+    },
+    action: {
+      type: Function,
+      default: () => ({}),
+    },
+  },
+  emits: ['closed'],
+  setup() {
+    const close = () => this.$emit('closed')
+
+    return { close }
+  },
+
+}
+</script>
