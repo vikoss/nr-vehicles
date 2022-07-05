7ac8ebc93b11da2516856a647ed2e45ecfc74c8d
vikoss <vikoss.yiss@gmail.com>
2022-07-04 23:29:57 -0500

    add web component for vehicle detail view

--- resources/js/views/Vehicle/Details.vue
+++ resources/js/views/Vehicle/Details.vue
@@ -1,79 +1,74 @@
 <template>
   <header-base />
   <main class="px-6 sm:px-16 py-12">
-    <redirect-to-back />
+    <redirect-to-back :route="app.goToVehicles" />
+    <title-bar
+      :title="app.vehicle.name"
+      subtitle="Consulta la información del vehiculo. De ser necesario puedes cargar documentación."
+    />
     <input-base
       id="inventory_number"
       v-model="app.vehicle.inventory_number"
       label="Número de inventario:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="name"
       v-model="app.vehicle.name"
       label="Nombre:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="brand"
       v-model="app.vehicle.brand"
       label="Marca:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="model"
       v-model="app.vehicle.model"
       label="Modelo:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="economic_number"
       v-model="app.vehicle.economic_number"
       label="Número economico:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="serial_number"
       v-model="app.vehicle.serial_number"
       label="Número de serie:"
       :disabled="true"
+      class="mb-2"
     />
     <input-base
       id="responsible_area"
       v-model="app.vehicle.responsible_area"
       label="Área responsable:"
       :disabled="true"
+      class="mb-2"
     />
-    <div>
-      <img src="https://adrianalonso.es/wp-content/uploads/2018/01/vue.jpg" alt="imagen">
-    </div>
-    <p class="mt-8 mb-4">Selecciona una imagen para verificar la información.</p>
-    <div class="flex">
-      <div class="mr-12">
-        <div class="bg-gray-200 h-32 w-28 flex justify-center items-center cursor-pointer">
-          <img src="https://adrianalonso.es/wp-content/uploads/2018/01/vue.jpg" alt="imagen">
-        </div>
-        <p class="text-wine text-base font-normal mt-4">Factura</p>
-      </div>
-      <div class="mr-12">
-        <div class="bg-gray-200 h-32 w-28 flex justify-center items-center cursor-pointer">
-          <img src="https://adrianalonso.es/wp-content/uploads/2018/01/vue.jpg" alt="imagen">
-        </div>
-        <p class="text-wine text-base font-normal mt-4">Factura</p>
-      </div>
-      <div class="mr-12">
-        <div class="bg-gray-200 h-32 w-28 flex justify-center items-center cursor-pointer">
-          <img src="https://adrianalonso.es/wp-content/uploads/2018/01/vue.jpg" alt="imagen">
-        </div>
-        <p class="text-wine text-base font-normal mt-4">Factura</p>
-      </div>
-      <div class="mt-12">
-        <arrow-right class="cursor-pointer h-9" />
-      </div>
-    </div>
-    <button-base
-      class="sm:max-w-sm mx-auto"
+    <!-- <image-viewer
+      v-if="!!app.documents.length"
+      :documents="app.documents"
+    /> -->
+    <!-- <button-base
+      class="sm:max-w-sm mx-auto mt-20"
       label="Agregar documentación"
+      :loading="false"
+      @click="app.goToUploadVehicleDocuments"
+      :disabled="false"
+    /> -->
+    <button-base
+      class="sm:max-w-sm mx-auto mt-8"
+      label="Consultar documentación"
       :loading="false"
       @click="app.goToUploadVehicleDocuments"
       :disabled="false"
@@ -84,7 +79,8 @@
 <script>
 import { computed, reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { searchVehicles, getVehicle } from '../../api/vehicles'
+import { getVehicle } from '../../api/vehicles'
+import { getDocuments } from '../../api/documents'
 import HeaderBase from '../../components/HeaderBase.vue'
 import SearchBar from '../../components/SearchBar.vue'
 import TableBase from '../../components/TableBase.vue'
@@ -92,9 +88,11 @@ import RedirectToBack from '../../components/RedirectToBack.vue'
 import InputBase from '../../components/InputBase.vue'
 import ButtonBase from '../../components/ButtonBase.vue'
 import ArrowRight from '../../svg/ArrowRight.vue'
+import ImageViewer from '../../components/ImageViewer.vue'
+import TitleBar from '../../components/TitleBar.vue'
 
 export default {
-  components: { HeaderBase, SearchBar, TableBase, RedirectToBack, InputBase, ArrowRight, ButtonBase },
+  components: { HeaderBase, SearchBar, TableBase, RedirectToBack, InputBase, ArrowRight, ButtonBase, ImageViewer, TitleBar, },
   setup() {
     const router = useRouter()
     const route = useRoute()
@@ -108,17 +106,25 @@ export default {
         }
         return false
       }),
-      getVehicles: async () => {
-        app.vehicles = await searchVehicles(app.value)
+      fetchDocuments: async () => {
+        const { data } =  await getDocuments(route.params.vehicle)
+        app.documents = data.map((document) => ({
+          type: document.document_type.name,
+          description: document.description,
+          url: document.url,
+        }))
       },
       fetchVehicle: async () => {
         app.vehicle = await getVehicle(route.params.vehicle)
       },
       goToUploadVehicleDocuments: () => router.push({ name: 'VehicleDocuments', params: { vehicle: route.params.vehicle }}),
+      goToVehicles: () => router.push({ name: 'Search' }),
       vehicle: {},
+      documents: [],
     })
 
     app.fetchVehicle()
+    app.fetchDocuments()
 
     return { app }
   },
