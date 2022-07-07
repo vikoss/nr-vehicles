fe90dd070f83738420545249a0ab74778b1e1ec7
vikoss <vikoss.yiss@gmail.com>
2022-07-06 22:57:18 -0500

    add fillable property and direction relationship in vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -10,10 +10,33 @@ class Vehicle extends Model
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'inventory_number',
+        'name',
+        'brand',
+        'model',
+        'economic_number',
+        'serial_number',
+        'direction_id',
+    ];
+
+    /**
      * Get the documents for the vehicle.
      */
     public function documents()
     {
         return $this->hasMany(Document::class);
+    }
+
+    /**
+     * Get the direction associated with the vehicle.
+     */
+    public function direction()
+    {
+        return $this->belongsTo(Direction::class);
     }
 }
