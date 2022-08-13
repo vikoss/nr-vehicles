02f6bc825eb8ff958ecc66221babfff3217105e2
vikoss <vikoss.yiss@gmail.com>
2022-08-12 19:06:15 -0500

    add PDF svg to assets

---
+++ resources/js/svg/PDF.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44">
+    <g id="Grupo_19416" data-name="Grupo 19416" transform="translate(15291 -10877.14)">
+      <g id="Grupo_19043" data-name="Grupo 19043" transform="translate(-15291 10877.198)">
+        <rect id="Rectángulo_8459" data-name="Rectángulo 8459" width="44" height="44" transform="translate(0 -0.059)" fill="none"/>
+      </g>
+      <path id="picture_as_pdf_FILL0_wght400_GRAD0_opsz48" d="M13.517,21.024h1.4V17.877h1.82a1.361,1.361,0,0,0,1.4-1.4v-1.82a1.361,1.361,0,0,0-1.4-1.4H13.517Zm1.4-4.55v-1.82h1.82v1.82Zm4.891,4.55H23a1.38,1.38,0,0,0,.986-.4,1.33,1.33,0,0,0,.417-1V14.654a1.33,1.33,0,0,0-.417-1,1.38,1.38,0,0,0-.986-.4H19.812Zm1.4-1.4V14.654H23v4.967Zm5.043,1.4h1.4V17.877h1.9v-1.4h-1.9v-1.82h1.9v-1.4h-3.3ZM10.825,29.782A2.333,2.333,0,0,1,8.55,27.507V6.275a2.182,2.182,0,0,1,.683-1.592A2.183,2.183,0,0,1,10.825,4H32.059a2.183,2.183,0,0,1,1.593.682,2.182,2.182,0,0,1,.683,1.592V27.507a2.333,2.333,0,0,1-2.275,2.275Zm0-2.275H32.059V6.275H10.825V27.507Zm-4.55,6.825a2.183,2.183,0,0,1-1.593-.682A2.182,2.182,0,0,1,4,32.057V8.55H6.275V32.057H29.784v2.275Zm4.55-28.057h0V27.507h0V6.275Z" transform="translate(-15287.801 10880.199)" :fill="hexColor"/>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'DocumentSVG',
+  props: {
+    hexColor: {
+      type: String,
+      default: '#54191b',
+    },
+  },
+}
+</script>
