961fccba17c39bed8c33bc48e037bbe6f11d8e61
vikoss <vikoss.yiss@gmail.com>
2022-08-12 19:06:52 -0500

    add helper for detect user agent mobile

---
+++ resources/js/helpers/utilities.js
@@ -0,0 +1,9 @@
+const isMobile = () => {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    return true
+  }
+
+  return false
+}
+
+export { isMobile }
