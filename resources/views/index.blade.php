36d868cb008f48735d30f90fea99d70d89149223
vikoss <vikoss.yiss@gmail.com>
2024-01-22 11:45:20 -0600

    build: change asset for secure asset function

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="google" content="notranslate">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <title>Vehículos - Nicolás Romero</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 </head>
 <body>
     <div id="app"></div>
